54ced39ea536d7d3b650ff933170677b80be45d9
mufasai <musafadil03@gmail.com>
2025-01-16 11:55:32 +0700

    fix: display

--- src/components/parameter-management/ParameterManagement.tsx
+++ src/components/parameter-management/ParameterManagement.tsx
@@ -208,7 +208,6 @@ const ParameterManagement = () => {
 
   return (
     <div class=" max-w-screen mx-auto">
-      {/* Header */}
       {/* Main Content Grid */}
       <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
         {/* Left Section - 2 columns */}
