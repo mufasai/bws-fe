c2fb4e4e97141461f76cd1c51c5ea2f9ab15e068
dzakyfadhilp <dzakyfadhilp@gmai.com>
2025-01-23 14:59:24 +0700

    fix layout

--- src/pages/SpvDashboard.tsx
+++ src/pages/SpvDashboard.tsx
@@ -129,7 +129,7 @@ const SpvDashboard = () => {
   ];
 
   return (
-    <div class="min-h-screen mt-6 ">
+    <div class="min-h-screen mt-6 m-6">
       <div class="max-w-screen mx-auto">
         <div class="space-y-6">
           {/* Reports Section */}
